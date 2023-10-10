2ae922f4ac52d3049026da3fef41c9f1a20c5ac3
Rami Alrabi <77370385+Strutio@users.noreply.github.com>
2023-10-10 14:25:28 -0400

    Update SpotifyPlaylist.py
    Added methods that retrieve the necessary data to populate the playlists

--- SpotifyPlaylist.py
+++ SpotifyPlaylist.py
@@ -5,6 +5,53 @@ from spotipy.oauth2 import SpotifyOAuth
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+def get_artist_id(sp, artist_name):
+    # Search for the artist
+    result = sp.search(artist_name, type='artist')
+    
+    # Extract and return the artist ID from the first result
+    return result['artists']['items'][0]['id']
+
+def get_similar_artists(sp, artist_id):
+    # Get similar artists
+    result = sp.artist_related_artists(artist_id)
+    
+    # Extract and return the artist IDs
+    return [artist['id'] for artist in result['artists']]
+
+def get_artist_top_tracks(sp, artist_ids, max_songs):
+    track_ids = []
+    
+    for artist_id in artist_ids:
+        # Get the artist's top tracks
+        results = sp.artist_top_tracks(artist_id)
+        
+        # Extract the track IDs
+        for track in results['tracks']:
+            if len(track_ids) < max_songs:
+                track_ids.append(track['id'])
+            else:
+                break
+
+        # If we already have max_songs, no need to keep looking.
+        if len(track_ids) >= max_songs:
+            break
+
+    return track_ids
+
+def create_playlist(sp, username, playlist_name, track_ids):
+    # Create the playlist
+    playlist = sp.user_playlist_create(username, playlist_name)
+    playlist_id = playlist['id']
+    
+    # Add the tracks in chunks of 100
+    for i in range(0, len(track_ids), 100):
+        sp.user_playlist_add_tracks(username, playlist_id, track_ids[i:i+100])
+    
+    return playlist_id
+
+
+
 def main(sp, username, artist_name, playlist_name, num_songs):
     # Get the artist's ID
     artist_id = get_artist_id(sp, artist_name)
