6fa8d91b5fbb896ba390514d254e704587d40f18
Rami Alrabi <77370385+Strutio@users.noreply.github.com>
2023-10-13 17:03:21 -0400

    Create frontend.js
    JS code that interacts with our html code

---
+++ frontend.js
@@ -0,0 +1,26 @@
+document.getElementById('playlist-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    fetch('http://localhost:5000/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'artist_name': document.getElementById('artist_name').value,
+            'playlist_name': document.getElementById('playlist_name').value,
+            'num_songs': document.getElementById('num_songs').value
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert('Playlist created with ID: ' + data.playlist_id);
+
+        // Assuming you have an iframe with id='spotify-embed' in your HTML
+        var spotifyEmbed = document.getElementById('spotify-embed');
+        spotifyEmbed.src = `https://open.spotify.com/embed/playlist/${data.playlist_id}`;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+});
